7f4611a0f8d187f912fcd15c3b46d8c0c3854648
Aguluka <melissacinta@gmail.com>
2019-09-28 00:53:33 +0100

    created dashboard and arranged files

---
+++ user/dashboard.php
@@ -0,0 +1,49 @@
+<div class="col-sm-12 segoe">
+    <div class="col-sm-10 col-sm-offset-1" style="overflow:hidden; margin-top: 10px; border: 1px solid #F8F8F8; padding:10px; border-radius: 10px">
+        <div class="right " style="float:left">
+            <p>Total amount budgeted</p>
+            <h1 style="color: #1032DE">&#8358;1,500,000.00</h1>
+        </div>
+        <div class="left" style="float:right">
+            <img src="../images/undraw_wallet_aym5 (1).png" alt="" width="120px;">
+        </div>
+    </div>
+    <div class="clear" style="clear:both"></div>
+    <div class="col-sm-10 col-sm-offset-1 table-responsive" style="margin-top: 10px;">
+        <table class="table table-striped">
+            <tr style="background-color:blue; ">
+                <th>Budgets</th>
+                <th>Number of items</th>
+                <th>Amount</th> 
+            </tr>
+            <tr>
+                <td>Grandma's birthday</td>
+                <td>15</td>
+                <td class="third">400,000.00</td>
+            </tr>
+            <tr>
+                <td>Laptop</td>
+                <td>6</td>
+                <td class="third">100,000.00</td>
+            </tr>
+            <tr>
+                <td>School fees</td>
+                <td>13</td>
+                <td class="third">270,000.00</td>
+            </tr>
+            <tr>
+                <td>House rent</td>
+                <td>11</td>
+                <td class="third">800,000.00</td>
+            </tr>
+            <tr>
+                <td>Mercy's allowance</td>
+                <td class="">2</td>
+                <td class="third">30,000.00</td>
+            </tr>
+        </table>
+    </div>
+    <div class="col-sm-10 col-sm-offset-1" style="margin-top: 10px;">
+        <button class="btn btn-infos">Create New Budget</button>
+    </div>
+</div>
