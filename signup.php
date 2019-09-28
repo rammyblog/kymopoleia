d0e4a4dc3ba6801e9d75f0db5e8a0ee60fe1aa7f
lemuel <lemuelizrael@gmail.com>
2019-09-28 16:36:33 +0100

    updated sign in and login
    updated sign in and login
    -added top left logo
    -added text (new to Kymo...) to login page
    _added text to (already have....) to sign up page
    -edited forgot password

--- signup.php
+++ signup.php
@@ -66,60 +66,73 @@ if($user){
 }
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/account.css">  
+    <link rel="stylesheet" href="css/account.css">
     <script src="https://kit.fontawesome.com/833e0cadb7.js" crossorigin="anonymous"></script>
     <title>KymoMoney | Register</title>
 </head>
+
+
+
+
+
 <body>
+
+    <!-- Just an image -->
+    <a href="index.php" id="top-logo"><img src="images/kymo.png" class="img-fluid" width="" height="30" alt=""></a>
+
+
 
     <img src="images/Ellipse.png" class="img-fluid top-ellipse" alt="">
     <section class="container login-section ">
         <div class="text-center spacing">
-        <a href="index.php"><img src="images/kymo.png" class="img-fluid" alt=""></a>
-    </div>
-        <h3 class="text-center spacing">Signup</h3>
-            <form class="" action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>' method="POST">
-                <div class="form-group col-md-4 ">
-                <input type="text" class="form-control" id="firstname" name="firstname" placeholder="Firstname" value="<?php echo $firstname; ?>"  required><span class="error"><?php echo $firstError; ?></span>
-                </div>
-                <div class="form-group col-md-4 ">
-                <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Lastname" value="<?php echo $lastname; ?>"  required><span class="error"><?php echo $lastError; ?></span>
-               
-                      
-                </div>
-                <div class="form-group col-md-4" >
+            <!-- <a href="index.php"><img src="images/kymo.png" class="img-fluid" alt=""></a> -->
+        </div>
+        <h3 class="text-center signUP spacing">Signup</h3>
+        <form class="form-align" action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>' method="POST">
+            <div class="form-group col-md-4 ">
+                <input type="text" class="form-control" id="firstname" name="firstname" placeholder="Firstname" value="<?php echo $firstname; ?>" required><span class="error"><?php echo $firstError; ?></span>
+            </div>
+            <div class="form-group col-md-4 ">
+                <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Lastname" value="<?php echo $lastname; ?>" required><span class="error"><?php echo $lastError; ?></span>
+
+
+            </div>
+            <div class="form-group col-md-4">
                 <input type="text" class="form-control" aria-describedby="usernameHelp" placeholder="Your username" id="username" name="username" value="<?php echo $username; ?>" required><span class="error"><?php echo $nameError; ?></span>
-                </div>
-                <div class="form-group col-md-4 ">
-                  <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Your email address" id="emailAddress" name="emailAddress" value="<?php echo $emailAddress; ?>" required><span class="error"><?php echo $emailError; ?></span>
-                </div>
+            </div>
+            <div class="form-group col-md-4 ">
+                <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Your email address" id="emailAddress" name="emailAddress" value="<?php echo $emailAddress; ?>" required><span class="error"><?php echo $emailError; ?></span>
+            </div>
 
-                <div class="form-group col-md-4">
-                  <input class="form-control" type="password"name="password" id="password"  class="form-control" placeholder="Your password" required><span class="error"><?php echo $passError; ?></span>
-                </div>
-                <div class="form-group col-md-4">
-                <input type="password"  class="form-control" name="confirmPassword" id="confirmPassword" placeholder="Confirm password" required ><span class="error"><?php echo $passError; ?></span>
-                </div>  
-                <button type="submit" class="btn btn-primary login-btn">Sign Up</button>
-            </form>   
-            <span class="error"><?php echo $errors; ?></span>
+            <div class="form-group col-md-4">
+                <input class="form-control" type="password" name="password" id="password" class="form-control" placeholder="Your password" required><span class="error"><?php echo $passError; ?></span>
+            </div>
+            <div class="form-group col-md-4">
+                <input type="password" class="form-control" name="confirmPassword" id="confirmPassword" placeholder="Confirm password" required><span class="error"><?php echo $passError; ?></span>
+            </div>
+            <button type="submit" class="btn btn-primary login-btn">Sign Up</button>
+
+            <p class="Already-acc">Already have an account?&nbsp;&nbsp;<span>Sign in</span></p>
+        </form>
+        <span class="error"><?php echo $errors; ?></span>
     </section>
-  
 
-    <img src="images/Ellipse.png" class="img-fluid bottom-ellipse d-none d-md-none d-lg-block" alt="">
-    
-    
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <img src="images/Ellipse.png" class="img-fluid bottom-ellipse" alt="">
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
+
 </html>
