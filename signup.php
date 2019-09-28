6fac3f57e2831063860578caec670d1071e2e8d0
ifejesu <ifejesugoodness97@gmail.com>
2019-09-29 00:16:00 +0100

    fixed menu

--- signup.php
+++ signup.php
@@ -80,7 +80,7 @@ if($user){
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="css/account.css">
         <script src="https://kit.fontawesome.com/833e0cadb7.js" crossorigin="anonymous"></script>
-        <title>KymoMoney | Register</title>
+        <title>KymoBudget | Register</title>
     </head>
 
 
