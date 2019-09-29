3c1dbbd63284d149be9146514a630c25b9de1324
emlycool <joshua.moshood@gmail.com>
2019-09-29 13:19:03 +0100

    ud_od code

--- addBudget.php
+++ addBudget.php
@@ -1,11 +1,63 @@
 <?php
-require_once "./PHP/database.php";
-session_start();
-if(!isset($_SESSION['user_id'])) {
-    header("location: login.php");
-    exit;
-}
+    ob_start();
+    session_start();
+    if(!isset($_SESSION['email'])){
+         header("Location: login.php");
+    }
+    
+    
+?>
 
+<?php
+require_once "./PHP/database.php";
+echo($_SESSION['usernames']);
+function protect_value($value){
+ $secured_value = trim(stripslashes(htmlentities($value)));     
+ return $secured_value;     
+}
+$data = array();
+$error = array();
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    //===CREATE BUDGET AND TOTAL AMOUNT
+    $total_amount = protect_value($_POST['amount']);
+    $budget_name = protect_value($_POST['budget_name']);
+    $startTime = protect_value($_POST['startTime']);
+    $endTime = protect_value($_POST['endTime']);
+    if (empty($total_amount)) {
+        $error['total_amount'] = "total_amount is required";
+    }
+    if (empty($budget_name)) {
+        $error['budget_name'] = "Budget name is required";
+    }
+    if (!filter_var($_POST['amount'], FILTER_VALIDATE_INT)) {
+        $error['total_amount'] = "Only in integers is accepted";
+    }
+    $sql = "SELECT * FROM budget WHERE Budget_id = '$budget_name' AND username = '{$_SESSION['usernames']}'";
+        $result = $conn->query($sql);
+    if ($result->fetch(PDO::FETCH_ASSOC)) {
+            $error['budget_name'] = "Budget exists with this name";
+    }
+    if (empty($error)) {
+        $insert = "INSERT INTO budget (id,Budget_id,Amount,startTime,endTime,username)
+                    VALUES( null, '$budget_name',  '$total_amount','$startTime','$endTime','{$_SESSION['usernames']}')";
+        $exe = $conn->exec($insert);
+        $_SESSION['Budget_id'] = $budget_name;
+        $_SESSION['Amount'] = $total_amount;
+        $data['message'] = "Budget created.";
+            
+    }
+    if ( !empty($error)) {
+        $data['success'] = false;
+        $data['errors']  = $error;
+    } else {
+        $data['success'] = true;
+        
+    }
+    // return to ajax
+    header("Location: addBudgetItems.php");
+    echo json_encode($data);
+    
+}
 ?>
 
 
@@ -16,11 +68,11 @@ if(!isset($_SESSION['user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://fonts.googleapis.com/css?family=Roboto|Rosarivo&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/add-budget.css">
+    <link rel="stylesheet" href="css/addBudget.css">
     <script src="https://kit.fontawesome.com/833e0cadb7.js" crossorigin="anonymous"></script>
     <link href="https://unpkg.com/bootstrap-table@1.15.4/dist/bootstrap-table.min.css" rel="stylesheet">
 
@@ -31,21 +83,21 @@ if(!isset($_SESSION['user_id'])) {
 <body class="">
     <header>
 
-        <nav>
+    <nav>
             <div class="brandname">
-                <h2 class="header-brandname"><a href="#"><img src="images/kymo.png" alt=""> </a></h2>
+                <h2 class="header-brandname"><a href="..index.php"><img src="images/kymo.png" alt=""> </a></h2>
             </div>
-            <p class="welcome_user">Hi, <span class="blueText">Femi Jeffery</span></p>
-            <i class="fa fa-user user-avatar"></i>
+            <p class="welcome_user">Hi, <span class="blueText"><?php echo $_SESSION['firstname']    ;  echo $_SESSION['lastname']   ; ?></span></p>
+            <img class='user-avatar' src="images/user.png" alt="">
             <div class="dropdown">
-                <div class="dropdown-toggler" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
-                    aria-expanded="false">
-                    <i class="fa fa-caret-down"></i>
-                </div>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="#">Logout</a>
-                </div>
-            </div>
+                    <div class="dropdown-toggler" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <img src="./images/drop.png" alt="">
+                    </div>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="dashboard.php"><?php  echo($_SESSION['usernames']); ?></a>
+                        <a class="dropdown-item" href="logout.php">Sign out</a>
+                    </div>
+                  </div>
 
         </nav>
 
@@ -53,202 +105,61 @@ if(!isset($_SESSION['user_id'])) {
 
     <main>
 
+
         <section class="sidebar">
+        
 
             <ul class="sidebar-list">
-                <li> <i class="fa fa-home"></i><a href="#" class="active"> Dashboard</a></li>
-                <li><i class="fa fa-file"></i><a href="#"> Create New Budget </a></li>
-                <li><i class="fa fa-plus-circle"></i><a href="#"> Add Budget </a></li>
-                <li><i class="fa fa-plus-circle"></i><a href="#"> Add Expenses</a></li>
+                <li><i class="fas fa-home"></i> Dashboard</li>
+                <li > <i class="fas fa-plus-circle"></i> View Budget Items</li>
+                <li class="active"><i class="fas fa-plus-circle"></i> Add Budget</li>
+                <li><i class="fas fa-plus-circle"></i>  Add Budget Items</li>
             </ul>
         </section>
 
-
-
-        <section class="buget__dashboard">
+        <section class="add-budget">
+        <div style="height: 100px"></div>
             <div class="container">
-
-                <div class="welcome__text">
-                    <div>
-                        <h4>Add Budget</h4>
-                        <p id="total_amount_budgeted"></p>
-                    </div>
-                    <div class="budget__form">
-                        <form action="">
-                            <input type="text" placeholder="Budget title">
-                            <input type="text" placeholder="Budget amount">
-                            <br>
-                            <input type="submit" value="Add">
-                        </form>
-                    </div>
-                </div>
-
-                <br><br>
-
-                <div class="welcome__text">
-                    <div>
-                        <h4>Add Budget Item</h4>
-                        <p id="total_amount_budgeted"></p>
-                    </div>
-                    <div class="budget__form">
-                        <form action="">
-                            <select name="item">
-                                <option value="volvo">Select Item</option>
-                                <option value="tax">Tax</option>
-                                <option value="rent">Rent</option>
-                                <option value="shopping">Shopping</option>
-                                <option value="foot">Food</option>
-                                <option value="drinks">Drinks</option>
-                                <option value="insurance">Insurance</option>
-                                <option value="allowance">Allowance</option>
-                                <option value="hotel">Hotel</option>
-                                <option value="transportation">Transportation</option>
-                                <option value="health">Health</option>
-                                <option value="baby">Baby</option>
-                                <option value="pet">Pet</option>
-                                <option value="investment">Investment</option>
-                                <option value="others">Others</option>
-                            </select>
-
-                            <select name="budget">
-                                <option value="">Select Budget</option>
-                                <option value="1">Saab</option>
-                                <option value="2">Fiat</option>
-                                <option value="3">Audi</option>
-                            </select>
-
-                            <br>
-                            <select name="priority">
-                                <option value="">Set Priority</option>
-                                <option value="3">High</option>
-                                <option value="2">Medium</option>
-                                <option value="1">Low</option>
-                            </select>
-                            <input type="text" placeholder="Description">
-                            <br>
-                            <input type="submit" value="Add">
-                        </form>
-                    </div>
-                </div>
-                <br><br><br><br>
-
-                <div>
-                    <div class="budget__headline">
-                        <div>
-                            <h5>Budget Title:</h5>
+                <form class="add-budget-form" action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>' method="POST">
+                    <h2 class="bgBlue">Add Budget</h2>
+                    <div class="form-row margin-height">
+                        <div class="form-group col-md-6 mTop">
+                            <input type="text" name="budget_name" id="budget_name" class="form-control" placeholder="Enter budget title">
                         </div>
-                        <div>
-                            <p>Grandma's Birthday</p>
+                        <div class="form-group col-md-6 mTop">
+                            <input type="text" name="amount" id="amount" class="form-control" placeholder="Enter amount">
                         </div>
                     </div>
-                    <br>
-                    <div class="budget__headline">
-                        <div>
-                            <h5>Amount Allocated:</h5>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <input type="date"  id="startTime" name="startTime" class="form-control" placeholder="Enter start time(dd/m/yy)" value=<?php
+                            $month = date('m');
+                            $day = date('d');
+                            $year = date('Y');
+                            $today = $year . '-' . $month . '-' . $day; echo $today;?>>
                         </div>
-                        <div>
-                            <p>&#8358; 1,500,000.00</p>
+                        <div class="form-group col-md-6">
+                            <input type="date" name="endTime" id="endTime" class="form-control" placeholder="Enter end time(dd/m/yy)">
                         </div>
                     </div>
-                    <br><br>
-                </div>
-
-                <table id="table" data-toggle="table" data-mobile-responsive="true" data-check-on-init="true">
-
-                    <thead>
-                        <tr>
-                            <th>S/N</th>
-                            <th>Item</th>
-                            <th>Description</th>
-                            <th>Priority</th>
-                            <th>Amount</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr id="tr-id-1" class="tr-class-1" data-title="bootstrap table" data-object='{"key": "value"}'>
-                            <td>1</td>
-                            <td data-value="526">Cake</td>
-                            <td data-value="">3 Tier Cake</td>
-                            <td>High</td>
-                            <td class="amount__budgeted" data-value="50000">₦50,000</td>
-                            <td><a href="#"><i class="fa fa-trash"></i></a></td>
-                        </tr>
-                        <tr id="tr-id-1" class="tr-class-1" data-title="bootstrap table" data-object='{"key": "value"}'>
-                            <td>1</td>
-                            <td data-value="526">Cake</td>
-                            <td data-value="">3 Tier Cake</td>
-                            <td>High</td>
-                            <td class="amount__budgeted" data-value="50000">₦50,000</td>
-                            <td><a href="#"><i class="fa fa-trash"></i></a></td>
-
-                        </tr>
-                        <tr id="tr-id-1" class="tr-class-1" data-title="bootstrap table" data-object='{"key": "value"}'>
-                            <td>1</td>
-                            <td data-value="526">Cake</td>
-                            <td data-value="">3 Tier Cake</td>
-                            <td>High</td>
-                            <td class="amount__budgeted" data-value="50000.50">₦50,000</td>
-                            <td><a href="#"><i class="fa fa-trash"></i></a></td>
-
-                        </tr>
-                        <tr id="tr-id-1" class="tr-class-1" data-title="bootstrap table" data-object='{"key": "value"}'>
-                            <td>1</td>
-                            <td data-value="526">Cake</td>
-                            <td data-value="">3 Tier Cake</td>
-                            <td>High</td>
-                            <td class="amount__budgeted" data-value="50000" data-data-value="50000">₦50,000</td>
-                            <td><a href="#"><i class="fa fa-trash"></i></a></td>
-
-                        </tr>
-                        <tr id="tr-id-1" class="tr-class-1" data-title="bootstrap table" data-object='{"key": "value"}'>
-                            <td>1</td>
-                            <td data-value="526">Cake</td>
-                            <td data-value="">3 Tier Cake</td>
-                            <td>High</td>
-                            <td class="amount__budgeted" data-value="50000">₦50,000</td>
-                            <td><a href="#"><i class="fa fa-trash"></i></a></td>
-
-                        </tr>
-                        <tr id="tr-id-1" class="tr-class-1" data-title="bootstrap table" data-object='{"key": "value"}'>
-                            <td>1</td>
-                            <td data-value="526">Cake</td>
-                            <td data-value="">3 Tier Cake</td>
-                            <td>High</td>
-                            <td class="amount__budgeted" data-value="50000">₦50,000</td>
-                            <td><a href="#"><i class="fa fa-trash"></i></a></td>
-
-                        </tr>
-
-                        <tr id="tr-id-1" class="tr-class-1" data-title="bootstrap table" data-object='{"key": "value"}'>
-                            <td>1</td>
-                            <td data-value="526">Cake</td>
-                            <td data-value="">3 Tier Cake</td>
-                            <td>High</td>
-                            <td class="amount__budgeted" data-value="50000">₦50,000</td>
-                            <td><a href="#"><i class="fa fa-trash"></i></a></td>
-
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-
+                    <button type="submit" class="btn budget-save text-center bgBlue">Create</button>
+                </form>
+            </div>    
         </section>
-
     </main>
 
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="dashboardNew.js"></script>
+    <script src="./js/dashboardNew.js"></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
     <script src="https://unpkg.com/bootstrap-table@1.15.4/dist/bootstrap-table.min.js"></script>
-    <script
-        src="https://unpkg.com/bootstrap-table@1.15.4/dist/extensions/mobile/bootstrap-table-mobile.min.js"></script>
+    <script src="https://unpkg.com/bootstrap-table@1.15.4/dist/extensions/mobile/bootstrap-table-mobile.min.js">
+    </script>
 
 </body>
 
